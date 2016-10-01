b2ab5410981119c1495d6df4eae12eab190adc9b
Brian <onnbrian@gmail.com>
2016-09-30 23:54:48 -0700

    Added example script using code

--- Kirkpatrick.py
+++ Kirkpatrick.py
@@ -270,18 +270,3 @@ class Kirkpatrick:
 	# draw point on fine graph to show location
 	def showPointOnGraph(self, q):
 		self.untouchedG.drawMeWithPoint(q)
-
-def main():
-
-	# randomly generate ~<numpoints>~ points and perform point location
-	numPoints = 50
-	points = [[randint(0, 100000), randint(0, 100000)] for i in xrange(numPoints)]
-	points.sort()
-	points = list(points for points,_ in itertools.groupby(points))
-
-	kp = Kirkpatrick(points)
-	query = [randint(0,100000), randint(0,100000)]
-	print kp.animatedLocation(query)
-
-if __name__ == "__main__":
-    main()
---
+++ example_main.py
@@ -0,0 +1,18 @@
+from random import randint
+import itertools
+from Kirkpatrick import Kirkpatrick
+
+def main():
+
+	# randomly generate ~<numpoints>~ points and perform point location
+	numPoints = 50
+	points = [[randint(0, 100000), randint(0, 100000)] for i in xrange(numPoints)]
+	points.sort()
+	points = list(points for points,_ in itertools.groupby(points))
+
+	kp = Kirkpatrick(points)
+	query = [randint(0,100000), randint(0,100000)]
+	print kp.animatedLocation(query)
+
+if __name__ == "__main__":
+    main()
